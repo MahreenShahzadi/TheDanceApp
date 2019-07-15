6e9ee66c83e5f4e271a4ccd011d8d55816c764d4
Coding Pursuits <codingpursuits@Codings-Mac.local>
2019-07-14 22:39:01 -0700

    Initial Commit

---
+++ TheDanceAppTests/TheDanceAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  TheDanceAppTests.swift
+//  TheDanceAppTests
+//
+//  Created by Coding Pursuits on 14/07/2019.
+//  Copyright © 2019 Coding Pursuits. All rights reserved.
+//
+
+import XCTest
+@testable import TheDanceApp
+
+class TheDanceAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
